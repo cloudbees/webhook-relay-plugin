23094a0006646436eefb1217b8d8861f3c356de0
Michael Pailloncy <mpailloncy@cloudbees.com>
2018-02-15 22:48:20 +0100

    OPS-3460 - Always disconnect connection during reconnect
    Even if URI specified is not empty, always kill current connection and
    create a new fresh one.

--- src/main/java/org/jenkinsci/plugins/webhookrelay/WebhookRelayManager.java
+++ src/main/java/org/jenkinsci/plugins/webhookrelay/WebhookRelayManager.java
@@ -6,11 +6,11 @@ import org.apache.commons.lang.StringUtils;
 
 
 public class WebhookRelayManager {
-    private static final Logger LOGGER = Logger.getLogger(WebhookRelayManager.class
-            .getName());
+
+    private static final Logger LOGGER = Logger.getLogger(WebhookRelayManager.class.getName());
 
     private static WebhookRelayManager instance = null;
-    private WebsocketHandler websocketHandler = new WebsocketHandler();
+    private WebsocketHandler websocketHandler = null;
 
     private WebhookRelayManager() {
     }
@@ -24,16 +24,17 @@ public class WebhookRelayManager {
 
     public void reconnect(String relayURI) {
 
-        if (StringUtils.isBlank(relayURI)) {
-            //this.bugsnag = null;
-            LOGGER.warning("WebhookRelay not connected as no relay URI configured");
+        if (websocketHandler != null) {
             websocketHandler.disconnectFromRelay();
+        }
+
+        if (StringUtils.isBlank(relayURI)) {
+            LOGGER.warning("WebhookRelay not connected as no relay URI configured");
             return;
         }
 
-        websocketHandler.connectToRelay(relayURI);
-
-        LOGGER.info("WebhookRelay exception handler has registered.");
+        websocketHandler = new WebsocketHandler(relayURI);
+        websocketHandler.connectToRelay();
     }
 
 }
