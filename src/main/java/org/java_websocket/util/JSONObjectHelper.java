ab0529bd872a36d9a0886e6bc1838f6c5055face
evildethow <evildethow@gmail.com>
2018-11-22 05:45:32 +1000

    CE-2284 - Decode payload if required

---
+++ src/main/java/org/java_websocket/util/JSONObjectHelper.java
@@ -0,0 +1,29 @@
+package org.java_websocket.util;
+
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+
+import javax.annotation.Nonnull;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class JSONObjectHelper {
+  
+  static final String ENCODING = "UTF-8";
+
+  public static @Nonnull JSONObject getJSONObjectDecodeSafe(@Nonnull String body) {
+    try {
+      return JSONObject.fromObject(body);
+    } catch (JSONException e) {
+      return JSONObject.fromObject(urlDecode(body));
+    }
+  }
+
+  private static @Nonnull String urlDecode(@Nonnull String pathToken) {
+    try {
+      return URLDecoder.decode(pathToken, ENCODING);
+    } catch (UnsupportedEncodingException e) {
+      throw new IllegalStateException(String.format("Unexpected URL decode exception. %s not supported on this system.", ENCODING), e);
+    }
+  }
+}
